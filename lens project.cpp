63aeaf9f58954e95db45ed3c46bc1ef36fa47095
Adefa Razka Rauhillah <rauhillahadefa794@gmail.com>
2026-05-02 19:19:55 +0700

    Refactor lens project.cpp with LinkedList class
    dua

--- lens project.cpp
+++ lens project.cpp
@@ -31,3 +31,36 @@ struct StackNode {
     string deskripsi;
     StackNode* next;
 };
+
+class LinkedList {
+public:
+    Alat* head;
+    int   totalItem;
+
+    LinkedList() : head(nullptr), totalItem(0) {}
+
+    
+    Alat* insertNode(string id, string nama, string kategori,
+                     string spesifikasi, string lokasiRak,
+                     string status, int stok) {
+        Alat* baru = new Alat();
+        baru->id           = id;
+        baru->nama         = nama;
+        baru->kategori     = kategori;
+        baru->spesifikasi  = spesifikasi;
+        baru->lokasiRak    = lokasiRak;
+        baru->status       = status;
+        baru->stok         = stok;
+        baru->next         = nullptr;
+
+        if (head == nullptr) {
+            head = baru;
+        } else {
+            Alat* curr = head;
+            while (curr->next != nullptr)
+                curr = curr->next;
+            curr->next = baru;
+        }
+        totalItem++;
+        return baru;
+    }
